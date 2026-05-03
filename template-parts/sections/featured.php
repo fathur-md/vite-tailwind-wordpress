74afa22fd93e111ec9da7ce7dd3512a5e65a6303
fathur-md <fathurmuh@outlook.com>
2026-05-03 18:24:22 +0700

    feat: add custom featured section & base 3 colors main.css

--- template-parts/sections/featured.php
+++ template-parts/sections/featured.php
@@ -1,44 +1,68 @@
-<section id="featured" class="py-20 px-5 bg-white dark:bg-[#161618] flex flex-col animate-fade-in">
+<section id="featured" class="py-24 px-5 bg-white dark:bg-[#161618] flex flex-col animate-fade-in">
     <div class="max-w-5xl mx-auto flex flex-col w-full">
         <div>
             <span class="text-sm md:text-xl font-medium text-gray-500 uppercase -tracking-tighter mb-1 block">
                 Featured
             </span>
-            <h2 class="text-2xl sm:text-3xl md:text-4xl font-semibold tracking-tight">
+            <h2 class="text-2xl sm:text-3xl md:text-5xl font-semibold tracking-tight">
                 Innovation in practice.
             </h2>
         </div>
 
-        <div class="bg-gray-100 dark:bg-slate-800 rounded-2xl relative shadow-md mt-10">
-            <div class="py-4 px-5 flex gap-2">
-                <span class="w-2.5 h-2.5 bg-notices-3 block rounded-full"></span>
-                <span class="w-2.5 h-2.5 bg-notices-5 block rounded-full"></span>
-                <span class="w-2.5 h-2.5 bg-green-500 block rounded-full"></span>
-            </div>
-            <div class="flex">
+        <div class="w-full">
+            <div class="bg-gray-100 dark:bg-slate-800 rounded-2xl relative shadow-md mt-10 overflow-hidden">
+                <div class="py-4 px-5 flex gap-2">
+                    <span class="w-2.5 h-2.5 bg-notices-3 block rounded-full"></span>
+                    <span class="w-2.5 h-2.5 bg-notices-5 block rounded-full"></span>
+                    <span class="w-2.5 h-2.5 bg-green-500 block rounded-full"></span>
+                </div>
                 <img
                     src="<?php echo get_img_url(30, 'full'); ?>" alt="hoaxai"
                     class="block">
             </div>
-
-            <div class="p-5 space-y-4 hidden">
-                <h2 class="text-3xl md:text-6xl text-blue-400 font-semibold w-full">
-                    HoaxDetect AI
-                </h2>
-                <p class="text-gray-300 text-base md:text-xl font-light max-w-3xl">
-                    A sophisticated SaaS-based fake news detection platform. Built as a prototype to empower digital literacy by verifying article authenticity in real-time.
-                </p>
-            </div>
-
         </div>
+
+
     </div>
 </section>
 
-<!-- <div class="max-w-5xl mx-auto w-full py-10 mt-10 space-y-2 text-center">
-    <h2 class="text-3xl md:text-6xl text-accent-1 font-semibold w-full">
-        HoaxDetect AI
-    </h2>
-    <p class="text-contrast-4 text-base md:text-xl font-light">
-        A sophisticated misinformation detection engine built to verify digital content authenticity using Gemini AI.
-    </p>
-</div> -->
+<section class="bg-white flex flex-col pb-4">
+    <!-- Mockup -->
+    <div class="bg-base-3 max-w-432 max-[1728px]:rounded-none mx-auto rounded-4xl overflow-hidden relative w-full">
+        <div class="flex flex-col pt-20 min-[1200px]:pb-170 justify-center items-center relative z-10">
+            <h1 class="text-center text-3xl sm:text-5xl font-semibold">
+                Design with Intent. Nothing More.
+            </h1>
+            <p class="text-xl mt-8 max-w-3xl mx-auto text-center font-medium sm:font-medium pb-8 px-5">
+                Stripping away the noise to focus on what truly matters: the truth.
+            </p>
+        </div>
+
+        <img
+            src="<?php echo get_img_url(36, 'full') ?>"
+            alt="hoaxai mobile"
+            class="relative min-[1200px]:absolute max-w-5xl bottom-0 left-0 z-10 w-full">
+
+        <img
+            src="<?php echo get_img_url(34, 'full') ?>"
+            alt="hoaxai mobile"
+            class="absolute max-w-100 bottom-0 right-0 hidden min-[1200px]:block">
+    </div>
+
+    <div class="px-4 mt-4">
+        <div class="bg-base-3 mx-auto py-12 w-full flex flex-col rounded-md">
+            <h1 class="text-center text-4xl md:text-5xl font-bold text-accent-1 px-5">Hoax Detect AI</h1>
+            <p class="text-base md:text-xl mt-4 max-w-3xl mx-auto text-center font-medium sm:font-medium pb-8 px-5">
+                A sophisticated SaaS-based fake news detection platform. Built as a prototype to empower digital literacy by verifying article authenticity in real-time.
+            </p>
+
+
+            <a href="https://fathur-md.github.io/hoaxdetect-ai-saas/" target="_blank" class="font-semibold group border-b-2 hover:border-accent-1 hover:text-accent-1 transition-colors duration-300 pb-1 mx-auto">
+                Launch Demo
+                <span class="inline-block group-hover:-rotate-45 transition-transform duration-300">
+                    →
+                </span>
+            </a>
+        </div>
+    </div>
+</section>
